1f8c1399d4f9f050fc1028a3d9962ca13aa7fbab
lekht <aleksa.skop2000@mail.ru>
2022-12-30 11:53:39 +0300

    schema fix

--- schema.sql
+++ schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE posts (
     author_id INTEGER REFERENCES authors(id) NOT NULL,
     title TEXT  NOT NULL,
     content TEXT NOT NULL,
-    created_at BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    created_at BIGINT NOT NULL DEFAULT extract(epoch from now())
 );
 
 INSERT INTO authors (id, name) VALUES (0, 'Дмитрий');
